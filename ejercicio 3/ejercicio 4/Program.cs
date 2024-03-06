8e7d7575443e8e1f8e322e372587c4cdab448679
Juan Pablo Tapeosi Chamo <52146627+juanpablo45875785@users.noreply.github.com>
2024-03-06 09:01:59 -0400

    tarea1

---
+++ ejercicio 3/ejercicio 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+public class oracion{
+    static void Main(string[]args){
+Console.WriteLine("Ingrese una oración:");
+        string oracion = Console.ReadLine();
+
+        int cantidadPalabras = ContarPalabras(oracion);
+
+        Console.WriteLine("La cantidad de palabras en la oración es: " + cantidadPalabras);
+    
+
+    static int ContarPalabras(string oracion)
+    {
+        // Dividir la oración en palabras utilizando los espacios como separadores
+        string[] palabras = oracion.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        // Retornar la cantidad de palabras obtenidas
+        return palabras.Length;
+    }
+}
+}
