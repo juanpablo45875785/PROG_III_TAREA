447e5eb29817beb3065f2a2485c6c5b08d6cb826
Juan Pablo Tapeosi Chamo <52146627+juanpablo45875785@users.noreply.github.com>
2024-03-06 09:01:01 -0400

    tarea1

---
+++ ejercicio 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Program
+{
+    static void Main(string[]args)
+    {
+        ContarVocales();
+    static void ContarVocales()
+    {
+    Console.WriteLine("Introduzca la palabra: ");
+    string palabra = Console.ReadLine().ToLower(); // Convertir la palabra a minúsculas para hacer coincidir las vocales
+    int conteoVocales = 0;
+    //int conteoVocales = palabra.Count(caracter => "aeiou".Contains(caracter)); // Contar las vocales en la palabra
+    for (int i = 0; i < palabra.Length; i++)
+    {
+        if ("aeiou".Contains(palabra[i]))
+        {
+            conteoVocales++; // Incrementar el contador si el caracter es una vocal
+        }
+    }
+    Console.WriteLine("La cantidad de vocales en la palabra es: " + conteoVocales);
+    Console.ReadKey();
+    }
+    }
+}
